fb3c1b13d516f13e13f52a19f362f6142161750b
KingOnHuy <kingponpawit@gmail.com>
2021-09-08 22:25:27 +0700

    Update no user select

--- src/App.vue
+++ src/App.vue
@@ -151,6 +151,16 @@ export default defineComponent({
 
 <style scoped>
 #app {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+}
+
+#app {
   @apply flex flex-col items-center h-full;
   height: 100%;
 }
