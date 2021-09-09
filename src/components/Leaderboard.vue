8884bcad0ff5357fa8578a40ff3c91391cfba8c7
Illyaz <26743376+illyaz@users.noreply.github.com>
2021-09-09 20:17:19 +0700

    fix here her pop style

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -140,8 +140,9 @@
         </div>
         <div class="footer text-center text-sm font-light pt-4 pb-4 ">
           <a
-            class="text-pink-500 font-bold text-base block"
+            class="text-pink-500 font-bold text-base block underline hover:text-pink-700"
             href="https://youtu.be/HUWyLcPf1vo?t=1766"
+            target="_blank"
           >
             HERE HER POP!
           </a>
